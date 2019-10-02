b45016427463d18b04d1d28dfa7660db40580cd1
yaya rizqi isnanda <yaya.isnanda@codemi.co.id>
2019-10-02 10:42:35 +0700

    go koans answer

---
+++ about_channels.go
@@ -0,0 +1,30 @@
+package go_koans
+
+func aboutChannels() {
+	ch := make(chan string, 2)
+
+	assert(len(ch) == 0) // channels are like buffers
+
+	ch <- "foo" // i mean, "metaphors are like similes"
+
+	assert(len(ch) == 1) // they can be queried for queued items
+
+	assert(<-ch == "foo") // items can be popped out of them
+
+	assert(len(ch) == 0) // and len() always reflects the "current" queue status
+
+	// the 'go' keyword runs a function-call in a new "goroutine"
+	// which executes "concurrently" with the calling "goroutine"
+	go func() {
+		// your code goes here
+		<-ch
+	}()
+
+	assert(len(ch) == 0) // we'll need to make room for the queue, or suffer deadlocks
+
+	ch <- "bar"   // this send will succeed
+	ch <- "quux"  // there's enough room for this send too
+	ch <- "extra" // but the buffer only has two slots
+
+	assert(len(ch) == 2)
+}
---
+++ about_common_interfaces.go
@@ -0,0 +1,37 @@
+package go_koans
+
+import "bytes"
+
+func aboutCommonInterfaces() {
+	{
+		in := new(bytes.Buffer)
+		in.WriteString("hello world")
+
+		out := new(bytes.Buffer)
+
+		in.WriteTo(out)
+
+		/*
+		   Your code goes here.
+		   Hint, use these resources:
+
+		   $ godoc -http=:8080
+		   $ open http://localhost:8080/pkg/io/
+		   $ open http://localhost:8080/pkg/bytes/
+		*/
+
+		assert(out.String() == "hello world") // get data from the io.Reader to the io.Writer
+	}
+
+	{
+		in := new(bytes.Buffer)
+		in.WriteString("hello world")
+
+		in.Truncate(5)
+
+		out := new(bytes.Buffer)
+		in.WriteTo(out)
+
+		assert(out.String() == "hello") // duplicate only a portion of the io.Reader
+	}
+}
